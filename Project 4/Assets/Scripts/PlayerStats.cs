d5fa43ab7f682125da3a5c60cc8f930a8e9b2318
Hailey Schindler <schindlerhailey@gmail.com>
2020-12-03 11:58:58 -0500

    12/3

---
+++ Project 4/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int playerHealth;
+    public int currentHealth;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
