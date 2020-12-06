7674d9df0d5b3717521b0ddc68847ac323ed16e2
EmmyGames <erickjlatshaw@gmail.com>
2020-12-06 15:36:05 -0500

    hopefully added hailey's stuff

---
+++ Assets/Scripts/RPGRelated/WeaponStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using TMPro;
+using UnityEngine.UI;
+
+public class WeaponStats : MonoBehaviour
+{
+    public Weapon weapon;
+    public Text AttackValue;
+    public Text DurabilityValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log(other.gameObject.name);
+            AttackValue.text = "Attack: " + weapon.attack.ToString();
+            DurabilityValue.text = "Durability: " + weapon.durability.ToString();
+        }
+    }
+} 
